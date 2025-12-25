118f3fa9b248c753bdd61512c72b838b263cb7f4
VIJAY S <vijaysivakadatcham@gmail.com>
2025-12-25 11:00:08 +0530

    created dynamic singup api

--- src/app.js
+++ src/app.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.use(express.json())
 
 
-app.use("/", (req,res)=> {
-  res.send("Hello World");
-})
+app.post("/signup", async (req, res) => {
+  try {
+    const userObj = req.body
+    // creating a new instance of the userMOdel
+    const user = new User(userObj);
+    await user.save();
 
+    res.status(201).json({
+      message: "User created successfully",
+      userId : user._id
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error creating user",
+      error: err.message,
+    });
+  }
+});
 
 const startServer = async () => {
-  try{
+  try {
     await connectDB();
-    console.log("Database connection established"); 
+    console.log("Database connection established");
     app.listen(3000, () => {
       console.log("server is successfully listening on server 3000 !!!");
     });
